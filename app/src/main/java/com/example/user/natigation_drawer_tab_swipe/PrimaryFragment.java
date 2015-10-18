cc67746edf4c0da5f8cad0eac04f0ac889b93645
playerlove1 <kos83611@gmail.com>
2015-10-18 15:48:33 +0800

    Initial commit

---
+++ app/src/main/java/com/example/user/natigation_drawer_tab_swipe/PrimaryFragment.java
@@ -0,0 +1,21 @@
+package com.example.user.natigation_drawer_tab_swipe;
+
+/**
+ * Created by user on 2015/10/17.
+ */
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class PrimaryFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.primary_layout,null);
+    }
+}
---
+++ app/src/main/java/com/example/user/natigation_drawer_tab_swipe/ThirdFragment.java
@@ -0,0 +1,19 @@
+package com.example.user.natigation_drawer_tab_swipe;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by user on 2015/10/18.
+ */
+public class ThirdFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.third_layout,null);
+    }
+}
